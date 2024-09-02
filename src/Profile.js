78c5dac4a8d0c7eab352c708a85fa2a99b6ac3ce
AlunoTI <AlunoTI@estacio.acad>
2024-09-02 20:26:08 -0300

    Ajustando condicional

--- src/Profile.js
+++
@@ -1,6 +0,0 @@
-import { useState } from "react";
-
-export default function Profile() {
-  const [name, setName] = useState("Paulo");
-  return <input value={name} onChange={(e) => setName(e.target.value)} />;
-}
